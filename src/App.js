5999a1ca6057a129c38fc913c00b9fae2940ec52
chandan pal <chandanpal18222gmail.com>
2024-07-11 20:57:07 +0530

    universal landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import AboutPace from './Components/AboutPace';
+import AboutUhs from './Components/AboutUhs';
+import Address from './Components/Address';
+import Carousel from './Components/carousal';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import image1 from './Components/Resources/image1.jpeg';
+import image2 from './Components/Resources/image2.jpeg';
+import image3 from  './Components/Resources/image3.jpeg';
+import image4 from './Components/Resources/image4.jpeg';
+import image5 from './Components/Resources/image5.jpeg';
+import image6 from './Components/Resources/image6.jpeg'
+import image7 from './Components/Resources/image4.jpeg';
+import image8 from './Components/Resources/image5.jpeg';
+import image9 from './Components/Resources/image6.jpeg'
+
 
 function App() {
+
+  const items = [
+    { image: image1,  },
+    { image: image2,  },
+    { image: image3,  },
+    { image: image4,  },
+    { image: image5,  },
+    { image: image6,  },
+    { image: image7,  },
+    { image: image8,  },
+    { image: image9,  },
+
+    // Add more items as needed
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <Header /> 
+        <AboutUhs/> 
+        <AboutPace/>
+        <Carousel items={items}/>
+        <Address/>
+        <Footer/>
     </div>
-  );
+);
 }
 
 export default App;
